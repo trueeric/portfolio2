21705cc8fe22e006477b0bf7e012d85985b223f6
Eric Uen <ikpassat@gmail.com>
2024-06-13 05:06:47 +0800

    portpolio2

---
+++ pages/about.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<p>This page will be displayed at the /about route.</p>
+	</div>
+	<div>
+		<button @click="enableCustomLayout">Update Layout</button>
+	</div>
+</template>
+
+<script setup>
+// definePageMeta({
+// 	layout: 'another',
+// })
+// function enableCustomLayout(){
+// 	setPageLayout(layout:'another')
+// }
+
+function enableCustomLayout() {
+	setPageLayout('another')
+}
+</script>
---
+++ pages/blog/index.vue
@@ -0,0 +1,3 @@
+<template>
+	<div>All blog posts listed !</div>
+</template>

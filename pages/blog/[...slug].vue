a9296910b44ebd7a5da1a5d34e52cbb1f71587fc
Eric Uen <ikpassat@gmail.com>
2024-06-15 07:40:16 +0800

    035_Table of Contents: Slots

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,8 +1,11 @@
 <template>
 	<article
-		class="prose dark:prose-invert prose-pre:bg-white dark:prose-pre:bg-gray-800 prose-pre:text-gray-700 dark:prose-pre:text-gray-300"
+		class="prose dark:prose-invert max-w-none prose-pre:bg-white dark:prose-pre:bg-gray-800 prose-pre:text-gray-700 dark:prose-pre:text-gray-300"
 	>
-		<ContentDoc />
+		<ContentDoc v-slot="{ doc }">
+			{{ doc.body.toc.links }}
+			<ContentRenderer: :value="doc"/>
+		</ContentDoc>
 	</article>
 </template>
 

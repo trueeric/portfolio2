c9e1bbcfd42f51d42536c89d4ab016157eb5a95e
Eric Uen <ikpassat@gmail.com>
2024-06-15 05:45:35 +0800

    034_Project Page in Markdown

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,8 +1,7 @@
 <template>
-	<div>
-		<h2 class="text-2xl font-semibold mb-10">Projects</h2>
-		<ProjectList />
-	</div>
+	<article class="prose dard:prose-invert">
+		<ContentDoc path="/projects" />
+	</article>
 </template>
 
 <script setup></script>

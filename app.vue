c07e9e30589b6dc93621bc6900debb0c7ffb8766
Eric Uen <ikpassat@gmail.com>
2024-06-17 22:03:44 +0800

    040_Page Transitions

--- app.vue
+++ app.vue
@@ -4,6 +4,14 @@
 	</NuxtLayout>
 </template>
 
-<!-- <script setup>
-const layout = 'default'
-</script> -->
+<style>
+.page-enter-active,
+.page-leave-active {
+	transition: all 0.3s;
+}
+.page-enter-from,
+.page-leave-to {
+	opacity: 0.5;
+	filter: grayscale(1);
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: {
+    enabled: true,
+    timeline:{ enabled: true}
+   },
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' }
+  },
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode', '@nuxt/content'],
   content:{
     highlight: {
